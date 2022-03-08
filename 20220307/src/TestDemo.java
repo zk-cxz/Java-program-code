6797d63c754a0f721c6242f82239c059deb3657f
蔡欣致 <2514029141@qq.com>
2022-03-08 23:13:09 +0800

    代码提交

---
+++ 20220307/src/TestDemo.java
@@ -0,0 +1,11 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        //psvm[main]
+        //sout[println]
+        System.out.println("123");
+        System.out.println("helloworld");
+
+
+
+    }
+}
