39d9cc7bfc9b57869b1f7b05950bdfd789937a02
蔡欣致 <2514029141@qq.com>
2022-03-09 21:23:58 +0800

    代码提交

--- 20220307/src/TestDemo.java
+++ 20220307/src/TestDemo.java
@@ -1,11 +1,23 @@
 public class TestDemo {
     public static void main(String[] args) {
+
+    }
+    public static void main2(String[] args) {
+        //方式一:在定义时给出初始值
+        int a=10;
+        System.out.println(a);
+        //方式二:在定义时没有给初始值，但使用前必须设置处置
+        int b;
+        b=10;
+        System.out.println(b);
+        //int型变量所能表示的范围:
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+    public static void main1(String[] args) {
         //psvm[main]
         //sout[println]
         System.out.println("123");
         System.out.println("helloworld");
-
-
-
     }
 }
