6244f3962fbc395e4eb4960d87cbc291e666b601
蔡欣致 <2514029141@qq.com>
2022-03-09 22:52:52 +0800

    代码提交

--- 20220307/src/TestDemo.java
+++ 20220307/src/TestDemo.java
@@ -1,6 +1,15 @@
 public class TestDemo {
     public static void main(String[] args) {
-
+        
+    }
+    public static void main3(String[] args) {
+        //long定义变量的三种情况:
+        long a=10;
+        long b=10L;
+        long c=10l;
+        //long型变量所能表示的范围:
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
     }
     public static void main2(String[] args) {
         //方式一:在定义时给出初始值
