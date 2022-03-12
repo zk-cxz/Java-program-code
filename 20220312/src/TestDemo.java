e240088abc1fe99c9a51fd0b830412a863f00276
蔡欣致 <2514029141@qq.com>
2022-03-12 22:27:23 +0800

    代码提交

---
+++ 20220312/src/TestDemo.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-03-12
+ * Time: 22:01
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main1(String[] args) {
+        System.out.println(10);     //输出带换行
+        System.out.print(10);       //输出不换行
+        System.out.printf("%d",10); //格式化输出
+    }
+}
