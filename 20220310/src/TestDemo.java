1cee769cc3a5999de7e62639c624bf581fd35f66
蔡欣致 <2514029141@qq.com>
2022-03-12 20:19:31 +0800

    代码提交

--- 20220310/src/TestDemo.java
+++ 20220310/src/TestDemo.java
@@ -7,6 +7,16 @@
  */
 public class TestDemo {
     public static void main(String[] args) {
+        int a=4;
+        if(3<a&&a<5){
+            System.out.println(a);
+        }
+    }
 
+    public static void main1(String[] args) {
+        int a=4;
+        if(3<a<5){
+            System.out.println(a);
+        }
     }
 }
