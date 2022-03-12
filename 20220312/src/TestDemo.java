655c8344fd5e29a26dbe40a3827561da97bbaecf
蔡欣致 <2514029141@qq.com>
2022-03-12 22:50:43 +0800

    代码提交

--- 20220312/src/TestDemo.java
+++ 20220312/src/TestDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -8,6 +10,27 @@
 public class TestDemo {
     public static void main(String[] args) {
 
+    }
+
+    public static void main3(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int num=sc.nextInt();
+            System.out.println(num);
+        }
+    }
+
+    public static void main2(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("请输入姓名:");
+        String name=sc.nextLine();
+        System.out.print("请输入年龄:");
+        int age=sc.nextInt();
+        System.out.print("请输入工资:");
+        float money=sc.nextFloat();
+        System.out.println("信息如下:");
+        System.out.println("姓名:"+name+"\n"+"年龄:"+age+"\n"+"工资:"+money);
+        sc.close();
     }
 
     public static void main1(String[] args) {
