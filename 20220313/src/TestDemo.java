683b95ca9054a9f4031ab7742c6d5fbdc13e2b9e
蔡欣致 <2514029141@qq.com>
2022-03-13 14:34:57 +0800

    代码提交

--- 20220313/src/TestDemo.java
+++ 20220313/src/TestDemo.java
@@ -8,8 +8,32 @@ import java.util.Scanner;
  * Time: 10:45
  */
 public class TestDemo {
-    //不使用第三变量交换变量值
+    /*满100全额打9折；
+    满500全额打8折；
+    满2000全额打7折；
+    满5000全额打6折；
+    且商场有抹零活动，不足一元的部分不需要付款（类型强制转换）
+    输入原来钱数，输出打折后的钱数*/
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        double num2=0;
+        if(num1<100){
+            num2=num1;
+        }else if(num1>=100&&num1<500){
+            num2=num1*0.9;
+        }else if(num1>=500&&num1<2000){
+            num2=num1*0.8;
+        }else if(num1>=2000&&num1<5000){
+            num2=num1*0.7;
+        }else if(num1>=5000){
+            num2=num1*0.6;
+        }
+        System.out.println((int)num2);
+    }
+
+    //不使用第三变量交换变量值
+    public static void main4(String[] args) {
         Scanner sc=new Scanner(System.in);
         int num1=sc.nextInt();
         int num2=sc.nextInt();
