0784adc497498a1efe71d3832059dd7c34e3fda7
蔡欣致 <2514029141@qq.com>
2022-03-13 14:23:57 +0800

    代码提交

--- 20220313/src/TestDemo.java
+++ 20220313/src/TestDemo.java
@@ -8,14 +8,26 @@ import java.util.Scanner;
  * Time: 10:45
  */
 public class TestDemo {
-    //输入一个浮点数，输出其四舍五入后的整数
+    //不使用第三变量交换变量值
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        num1^=num2;
+        num2^=num1;
+        num1^=num2;
+        System.out.println(num1+" "+num2);
+    }
+
+    //输入一个浮点数，输出其四舍五入后的整数
+    public static void main3(String[] args) {
         Scanner sc=new Scanner(System.in);
         double num=sc.nextDouble();
         if(num-(int)num>=0.5){
             num+=1;
         }
         System.out.println((int)num);
+        sc.close();
     }
 
     //输出两个整数的和，差，积，商，模
