b40115cf4360264f325c9f4f43b554569187cb7b
蔡欣致 <2514029141@qq.com>
2022-03-13 10:51:23 +0800

    代码提交

---
+++ 20220313/src/TestDemo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-03-13
+ * Time: 10:45
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+
+    //随机double类型变量转换成int类型
+    public static void main1(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double num=sc.nextDouble();
+        System.out.println((int)num);
+    }
+}
