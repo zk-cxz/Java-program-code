7381a0c4b17490ddfedb207c979edece20af0ada
蔡欣致 <2514029141@qq.com>
2022-03-13 10:58:43 +0800

    代码提交

--- 20220313/src/TestDemo.java
+++ 20220313/src/TestDemo.java
@@ -8,8 +8,17 @@ import java.util.Scanner;
  * Time: 10:45
  */
 public class TestDemo {
+    //输出两个整数的和，差，积，商，模
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        if(num1>=num2){
+            System.out.println((num1+num2)+" "+(num1-num2)+" "+(num1*num2)+" "+(num1/num2)+" "+(num1%num2));
+        }else{
+            System.out.println((num2+num1)+" "+(num2-num1)+" "+(num1*num2)+" "+(num2/num1)+" "+(num2%num1));
+        }
+        sc.close();
     }
 
     //随机double类型变量转换成int类型
@@ -17,5 +26,6 @@ public class TestDemo {
         Scanner sc=new Scanner(System.in);
         double num=sc.nextDouble();
         System.out.println((int)num);
+        sc.close();
     }
 }
