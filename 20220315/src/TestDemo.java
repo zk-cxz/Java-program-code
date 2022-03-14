62878da4f646dfe13b4ff1ba0212d311a0a2ee57
蔡欣致 <2514029141@qq.com>
2022-03-15 00:27:57 +0800

    代码提交

--- 20220315/src/TestDemo.java
+++ 20220315/src/TestDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -7,6 +9,19 @@
  */
 public class TestDemo {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        eat1(n);
+        sc.close();
+    }
 
+    public static void eat1(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.println("第一分钟");
+            System.out.println("第二分钟");
+            System.out.println("第三分钟");
+            System.out.println("第四分钟");
+            System.out.println("第五分钟");
+        }
     }
 }
