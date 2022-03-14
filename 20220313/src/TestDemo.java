c0782a703856d7984e751052abc8354d1780aa8b
蔡欣致 <2514029141@qq.com>
2022-03-14 15:56:44 +0800

    代码提交

--- 20220313/src/TestDemo.java
+++ 20220313/src/TestDemo.java
@@ -8,8 +8,25 @@ import java.util.Scanner;
  * Time: 10:45
  */
 public class TestDemo {
-    //求平均数
+    //判断质数
     public static void main(String[] args) {
+        TestDemo main = new TestDemo();
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println(main.isPrimeNumber(number));
+    }
+
+    public Boolean isPrimeNumber(int number) {
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //求平均数
+    public static void main13(String[] args) {
         Scanner sc=new Scanner(System.in);
         double sum=0;
         int count=0;
