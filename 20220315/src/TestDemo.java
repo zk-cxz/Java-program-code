7be09c9e17be792d3c738ad45db1f6b1b39d8aee
蔡欣致 <2514029141@qq.com>
2022-03-15 09:15:41 +0800

    代码提交

--- 20220315/src/TestDemo.java
+++ 20220315/src/TestDemo.java
@@ -11,8 +11,18 @@ public class TestDemo {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        eat1(n);
+        eat2(n);
         sc.close();
+    }
+
+    public static void eat2(int n) {
+        for (int i = n; i > 1; i/=2) {
+            System.out.println("第一分钟");
+            System.out.println("第二分钟");
+            System.out.println("第三分钟");
+            System.out.println("第四分钟");
+            System.out.println("第五分钟");
+        }
     }
 
     public static void eat1(int n){
