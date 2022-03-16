d1c2f4413454d8b54e98494e15ee5257d1f5f80c
蔡欣致 <2514029141@qq.com>
2022-03-16 22:28:00 +0800

    代码提交

--- 20220315/src/TestDemo.java
+++ 20220315/src/TestDemo.java
@@ -9,8 +9,13 @@ import java.util.Scanner;
  * Time: 16:05
  */
 public class TestDemo {
+    //提取不重复的整数
     public static void main(String[] args) {
-
+        Scanner scanner=new Scanner(System.in);
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
+        System.out.println(num1+num2);
+        scanner.close();
     }
 
     //取近似值
