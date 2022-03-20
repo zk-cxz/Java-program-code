8a9049001607d08e596545b29c2aa6deafb6c707
蔡欣致 <2514029141@qq.com>
2022-03-20 11:33:19 +0800

    代码提交

--- 20220320/src/TestDemo.java
+++ 20220320/src/TestDemo.java
@@ -6,17 +6,26 @@
  * Time: 10:31
  */
 public class TestDemo {
-    public static void print(int[] array,int len){
+    public static void print1(int[] array,int len){
         if(len==1){
             System.out.println(array[len-1]);
             return;
         }
-        print(array,len-1);
+        System.out.println(array[len-1]);
+        print1(array,len-1);
+    }
+
+    public static void print(int[] array,int len){
+        if(len==array.length){
+            System.out.println(array[len-1]);
+            return;
+        }
+        print(array,len+1);
         System.out.println(array[len-1]);
     }
 
     public static void main(String[] args) {
         int[] array={1,2,3,4,5};
-        print(array,array.length);
+        print(array,1);
     }
 }
