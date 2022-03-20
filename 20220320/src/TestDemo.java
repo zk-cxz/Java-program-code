f40eb3e9015b3112777d408ab7003b95c4ab6aba
蔡欣致 <2514029141@qq.com>
2022-03-20 17:23:54 +0800

    代码提交

--- 20220320/src/TestDemo.java
+++ 20220320/src/TestDemo.java
@@ -6,6 +6,24 @@
  * Time: 10:31
  */
 public class TestDemo {
+    public static String myToString(int[] arr){
+        String n="[";
+        for (int i = 0; i < arr.length; i++) {
+            n+=arr[i];
+            if(i!=arr.length-1){
+                n+=",";
+            }
+        }
+        n+="]";
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        String ret=myToString(arr);
+        System.out.println(ret);
+    }
+
     public static void print1(int[] array,int len){
         if(len==1){
             System.out.println(array[len-1]);
@@ -24,7 +42,7 @@ public class TestDemo {
         System.out.println(array[len-1]);
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         int[] array={1,2,3,4,5};
         print(array,1);
     }
