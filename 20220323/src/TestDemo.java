74ced7e96494cac509e92980312028fc4f753bde
蔡欣致 <2514029141@qq.com>
2022-03-23 21:03:18 +0800

    代码提交

--- 20220323/src/TestDemo.java
+++ 20220323/src/TestDemo.java
@@ -6,6 +6,25 @@
  * Time: 17:39
  */
 public class TestDemo {
+    public static boolean sort(int[] array){
+        for (int i = 0; i < array.length-1; i++) {
+            if(array[i]>array[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] array={2,8,4,7};
+        boolean ret=sort(array);
+        if(ret==true){
+            System.out.println("数组有序递增");
+        }else{
+            System.out.println("数组无序");
+        }
+    }
+
     //模拟实现二分查找
     public static int binarySearch(int[] array,int n){
         int left=0;
