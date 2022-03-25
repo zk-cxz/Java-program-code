7842deccee7efa990ef8e768c2cb76fcf1df905d
蔡欣致 <2514029141@qq.com>
2022-03-25 23:14:37 +0800

    代码提交

---
+++ 20220325/src/TestDemo.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-03-25
+ * Time: 15:11
+ */
+public class TestDemo {
+    public static void fuc1(int[] array){
+        array=new int[]{4,5,6};
+    }
+
+    public static void fuc(int[] array){
+        array[0]=2;
+    }
+
+    public static void main(String[] args) {
+        int[] array=new int[]{1,2,3};
+        fuc(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
