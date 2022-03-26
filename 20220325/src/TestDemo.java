ef3738b829b189e5b59488000b3ca6b04812f5be
蔡欣致 <2514029141@qq.com>
2022-03-26 15:06:24 +0800

    代码提交

--- 20220325/src/TestDemo.java
+++ 20220325/src/TestDemo.java
@@ -8,6 +8,10 @@ import java.util.Arrays;
  * Time: 15:11
  */
 public class TestDemo {
+    public static void main(String[] args) {
+        
+    }
+
     public static void fuc1(int[] array){
         array=new int[]{4,5,6};
     }
@@ -16,7 +20,7 @@ public class TestDemo {
         array[0]=2;
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         int[] array=new int[]{1,2,3};
         fuc(array);
         System.out.println(Arrays.toString(array));
