acbd4159adbd9844a3ac192695d99cbc709349b7
蔡欣致 <2514029141@qq.com>
2022-03-28 22:44:22 +0800

    代码提交

--- 20220328/src/TestDemo6.java
+++ 20220328/src/TestDemo6.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -6,4 +8,19 @@
  * Time: 22:34
  */
 public class TestDemo6 {
+    //去掉最低工资和最高工资后的工资平均值
+    public static double average(int[] salary) {
+        Arrays.sort(salary);
+        double sum=0;
+        for (int i = 1; i < salary.length-1; i++) {
+            sum+=salary[i];
+        }
+        return sum/(salary.length-2);
+    }
+
+    public static void main(String[] args) {
+        int[] array={4000,3000,1000,2000};
+        double ret=average(array);
+        System.out.println(ret);
+    }
 }
