a1acc122c872e04f82022e8b488b2187c4f7b3df
蔡欣致 <2514029141@qq.com>
2022-03-31 17:33:38 +0800

    代码提交

--- 20220331/src/TestDemo7.java
+++ 20220331/src/TestDemo7.java
@@ -10,6 +10,18 @@ class Dog{
     public int age;
     public String sex;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
     public void eat(){
         System.out.println(name+"正在吃饭");
     }
@@ -22,9 +34,12 @@ class Dog{
 public class TestDemo7 {
     public static void main(String[] args) {
         Dog dog1=new Dog();
-        dog1.name="小黑";
+        dog1.setName("小黑");
+        dog1.setAge(6);
+        dog1.setSex("男");
+        /*dog1.name="小黑";
         dog1.age=6;
-        dog1.sex="男";
+        dog1.sex="男";*/
         dog1.sleep();
     }
 }
