d844fd5766a6a9508d7c7c4033cd79f4a460d523
蔡欣致 <2514029141@qq.com>
2022-04-09 15:56:29 +0800

    代码提交

---
+++ 20220407/src/TestDemo2.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-04-09
+ * Time: 15:31
+ */
+public class TestDemo2 {
+    public static void main(String[] args) {
+        int[] array={3,4,1,8,5};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
