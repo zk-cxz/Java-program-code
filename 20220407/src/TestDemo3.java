fd334c9fdf7d918769d161d91daecc852c0996f0
蔡欣致 <2514029141@qq.com>
2022-04-09 17:55:18 +0800

    代码提交

---
+++ 20220407/src/TestDemo3.java
@@ -0,0 +1,15 @@
+import static java.lang.Math.*;
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-04-09
+ * Time: 15:56
+ */
+public class TestDemo3 {
+    public static void main(String[] args) {
+        //double ret=Math.sqrt(100);
+        double ret=sqrt(100);
+        System.out.println(ret);
+    }
+}
