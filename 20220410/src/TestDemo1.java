bc1264d2a6f1b53619bd1371d04d296ec84c74e2
蔡欣致 <2514029141@qq.com>
2022-04-10 11:26:53 +0800

    代码提交

---
+++ 20220410/src/TestDemo1.java
@@ -0,0 +1,31 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-04-10
+ * Time: 9:36
+ */
+class Outclass{
+    public static int a;
+    private int b;
+    class Innerclass{
+        int b;
+
+        public void func(){
+            a=100;
+
+        }
+    }
+}
+
+public class TestDemo1 {
+    public static void main(String[] args) {
+        //写法一：
+        /*Outclass outclass=new Outclass();
+        Outclass.Innerclass innerclass=outclass.new Innerclass();*/
+        //写法二：
+        Outclass.Innerclass innerclass=new Outclass().new Innerclass();
+        innerclass.func();
+        System.out.println(Outclass.a);
+    }
+}
