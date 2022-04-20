5ecce5ec7fa4d1248360b032d7926e05680ce240
蔡欣致 <2514029141@qq.com>
2022-04-20 11:47:19 +0800

    代码提交

--- 20220420/src/test2/TestDemo2.java
+++ 20220420/src/test2/TestDemo2.java
@@ -7,5 +7,24 @@ package test2;
  * Date: 2022-04-20
  * Time: 11:27
  */
+class B {
+    public B() {
+        func();
+    }
+    public void func() {
+        System.out.println("B.func()");
+    }
+}
+class D extends B {
+    private int num = 1;
+
+    @Override
+    public void func() {
+        System.out.println("D.func() " + num);
+    }
+}
+
 public class TestDemo2 {
+    D d=new D();
+    d.func();   //直接报错
 }
