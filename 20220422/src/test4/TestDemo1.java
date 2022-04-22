9e118f8aa57c98dfd16ecaab1111b3cd78eb4e7a
蔡欣致 <2514029141@qq.com>
2022-04-22 21:13:20 +0800

    代码提交

--- 20220422/src/test4/TestDemo1.java
+++ 20220422/src/test4/TestDemo1.java
@@ -7,8 +7,20 @@ package test4;
  * Date: 2022-04-22
  * Time: 20:10
  */
+interface I{
+    void func();
+}
+
+class A implements I{
+    @Override
+    public void func() {
+        System.out.println("666");
+    }
+}
+
 public class TestDemo1 {
     public static void main(String[] args) {
-
+        Object obj=new A();   //向上转型
+        A a=(A)obj;   //向下转型
     }
 }
