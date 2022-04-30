76b131f35b0077bbfc40c2a5fc3447ca996ea92f
蔡欣致 <2514029141@qq.com>
2022-04-30 20:49:58 +0800

    String类代码提交

---
+++ 20220430/src/test1/TestDemo2.java
@@ -0,0 +1,21 @@
+package test1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-04-30
+ * Time: 17:37
+ */
+public class TestDemo2 {
+    public static void main(String[] args) {
+
+        //String str4="abc";
+        String str1=new String("abc");
+        String str3="abc";
+        String str2=new String("abc");
+        System.out.println(str1==str2);   //false
+        System.out.println(str1.equals(str2));   //true
+        System.out.println(str1==str3);
+    }
+}
