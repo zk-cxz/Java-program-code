ef2c69fd0d562cdf8f667ae7544b5565841814b0
蔡欣致 <2514029141@qq.com>
2022-06-11 20:34:47 +0800

    堆（优先级队列）代码提交

--- 20220611/src/TestDemo.java
+++ 20220611/src/TestDemo.java
@@ -16,8 +16,39 @@ class IntCmp implements Comparator<Integer>{
 }
 
 public class TestDemo {
-    public static void main(String[] args) {
-
+    /**
+     * 查找最小的K个元素
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int[] smallestK(int[] arr, int k) {
+        if(k<=0){
+            return new int[k];
+        }
+        PriorityQueue<Integer> priorityQueue=new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        for(int i=0;i<arr.length;i++){
+            if(priorityQueue.size()<k){
+                priorityQueue.offer(arr[i]);
+            }else{
+                int top=priorityQueue.peek();
+                if(arr[i]<top){
+                    priorityQueue.poll();
+                    priorityQueue.offer(arr[i]);
+                }
+            }
+        }
+        int[] ret=new int[k];
+        for(int i=0;i<k;i++){
+            int val=priorityQueue.poll();
+            ret[i]=val;
+        }
+        return ret;
     }
 
     public static void main3(String[] args) {
