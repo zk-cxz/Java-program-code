37162831d9df6e00064301d537684b069dd5dfbe
蔡欣致 <2514029141@qq.com>
2022-06-24 10:57:34 +0800

    链表复盘代码提交

--- TestMySingleList/src/MySingleList.java
+++ TestMySingleList/src/MySingleList.java
@@ -411,6 +411,85 @@ public class MySingleList {
      * @return
      */
     public ListNode getIntersectionNode(ListNode headA,ListNode headB){
+        if(headA==null || headB==null){
+            return null;
+        }
+        int lenA=0;
+        int lenB=0;
+        ListNode pl=headA;
+        ListNode ps=headB;
+        while(pl!=null){
+            lenA++;
+            pl=pl.next;
+        }
+        while(ps!=null){
+            lenB++;
+            ps=ps.next;
+        }
+        pl=headA;
+        ps=headB;
+        int len=lenA-lenB;
+        if(len<0){
+            pl=headB;
+            ps=headA;
+            len=-len;
+        }
+        while(len>0){
+            pl=pl.next;
+            len--;
+        }
+        while(pl!=ps){
+            pl=pl.next;
+            ps=ps.next;
+        }
+        return pl;
+    }
 
+    /**
+     * 判断链表是否构成环
+     * @return
+     */
+    public boolean hasCycle(){
+        if(this.head==null){
+            return false;
+        }
+        ListNode slow=this.head;
+        ListNode fast=this.head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 查找进入环的节点
+     * @return
+     */
+    public ListNode detectCycle(){
+        if(this.head==null){
+            return null;
+        }
+        ListNode slow=this.head;
+        ListNode fast=this.head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                break;
+            }
+        }
+        if(fast==null || fast.next==null){
+            return null;
+        }
+        slow=this.head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
     }
 }
