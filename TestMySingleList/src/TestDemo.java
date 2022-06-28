35c87700b23b8eb3372f0cc0e9d85e52f12f774d
蔡欣致 <2514029141@qq.com>
2022-06-28 20:48:01 +0800

    链表复盘代码提交

---
+++ TestMySingleList/src/TestDemo.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-06-28
+ * Time: 15:18
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        List<Integer> list=new LinkedList<>();
+        list.clear();
+    }
+}
