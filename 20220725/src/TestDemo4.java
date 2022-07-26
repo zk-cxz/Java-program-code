2c5159795f0570c9754a25ac84655aaf78315f23
蔡欣致 <2514029141@qq.com>
2022-07-26 21:32:14 +0800

    Java多线程代码提交

--- 20220725/src/TestDemo4.java
+++ 20220725/src/TestDemo4.java
@@ -6,7 +6,7 @@
  * Time: 21:00
  */
 class A{
-    synchronized public void m1(String a){
+    synchronized public static void m1(String a){
         System.out.println(a+"开始m1");
         try {
             Thread.sleep(3000);
@@ -16,7 +16,7 @@ class A{
         System.out.println(a+"结束m1");
     }
 
-    synchronized public void m2(String a){
+    synchronized public static void m2(String a){
         System.out.println(a+"开始m2");
         try {
             Thread.sleep(3000);
@@ -30,16 +30,18 @@ class A{
 
 public class TestDemo4 {
     public static void main(String[] args) {
-        A a1=new A();
-        A a2=new A();
+        //A a1=new A();
+        //A a2=new A();
 
         Thread thread1=new Thread(() -> {
-            a1.m1("线程1");
+            //a1.m1("线程1");
+            A.m1("线程1");
         });
 
         Thread thread2=new Thread(() -> {
             //a2.m1("线程2");
-            a1.m2("线程2");
+            //a1.m2("线程2");
+            A.m2("线程2");
         });
 
         thread1.start();
