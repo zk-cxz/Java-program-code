4b77133aa6f32e63897461b7b442e56ad05dffd9
蔡欣致 <2514029141@qq.com>
2022-07-28 22:11:22 +0800

    Java多线程代码提交

---
+++ 20220728/src/Demo1.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-07-28
+ * Time: 12:47
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        BlockingQueue<Integer> queue=new LinkedBlockingDeque<>();
+
+        Thread customer=new Thread(() -> {
+            while(true){
+                try {
+                    int value=queue.take();
+                    System.out.println("消费元素:"+value);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        customer.start();
+
+        Thread producer=new Thread(() -> {
+            int n=0;
+            while(true){
+                try {
+                    System.out.println("生产元素:"+n);
+                    queue.put(n);
+                    n++;
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        producer.start();
+    }
+}
---
+++ eclipse/20220728/src/TestDemo1.java
@@ -0,0 +1,9 @@
+
+public class TestDemo1 {
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int c=5;
+		System.out.println(c+" "+"Hello world!"+" "+(a+b));
+	}
+}
