829a3ee70d8404b45f8b576b16aa0d99bdc05260
蔡欣致 <2514029141@qq.com>
2022-08-02 23:23:09 +0800

    文件操作代码提交

--- 20220802/src/Demo1.java
+++ 20220802/src/Demo1.java
@@ -17,15 +17,25 @@ public class Demo1 {
             inputStream=new FileInputStream("./test.txt");
 
             //2.读取文件
-            //法一:
-            while(true){
+            //法一:read()方法不带参数
+            /*while(true){
                 int b=inputStream.read();
                 if(b==-1){
                     //文件读完了
                     break;
                 }
                 System.out.println(b);
-            }
+            }*/
+
+            //法二:read()方法带一个参数
+            byte[] b=new byte[1024];
+            int len=inputStream.read(b);
+            /*System.out.println(len);
+            for(int i=0;i<len;i++){
+                System.out.println(b[i]);
+            }*/
+            String str=new String(b,0,len,"utf8");
+            System.out.println(str);
         }catch (IOException e){
             e.printStackTrace();
         }finally {
