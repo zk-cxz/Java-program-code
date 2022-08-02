d4b85b2b8e9de0812f450781bfd080e12d0b13ad
蔡欣致 <2514029141@qq.com>
2022-08-02 23:30:26 +0800

    文件操作代码提交

---
+++ 20220802/src/Demo3.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-08-02
+ * Time: 23:27
+ */
+public class Demo3 {
+    //对于文本读操作,最简单的写法:
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream=new FileInputStream("test.txt");
+        Scanner scanner=new Scanner(inputStream);
+        String str=scanner.next();
+        System.out.println(str);
+        inputStream.close();
+    }
+}
