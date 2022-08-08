e836524838ace62902fc40a19c2ca4f4b209b92c
蔡欣致 <2514029141@qq.com>
2022-08-08 16:27:28 +0800

    蓝桥杯2021 Java B组题目代码提交

---
+++ langqiaobei/src/Java_B_2021/B.java
@@ -0,0 +1,35 @@
+package Java_B_2021;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-08-08
+ * Time: 16:09
+ */
+public class B {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        for(int i = 0; i < 10; i++){
+            numbers[i] = 2021;
+        }
+        int number = 0;
+        boolean flag = true;
+        while(flag){
+            //将整数先转字符串后再转字符数组
+            char[] str = String.valueOf(number).toCharArray();
+            for(int i = 0; i < str.length; i++){
+                if(numbers[str[i]-'0'] > 0){
+                    numbers[str[i]-'0']--;
+                }else{
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                number++;
+            }
+        }
+        System.out.println(number-1);
+    }
+}
