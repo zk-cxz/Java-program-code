4fcfce0be50bcedb665162c9b765d77e85d54592
蔡欣致 <2514029141@qq.com>
2022-08-11 21:20:14 +0800

    蓝桥杯2021 Java B组题目代码提交

--- langqiaobei/src/Java_B_2021/D.java
+++ langqiaobei/src/Java_B_2021/D.java
@@ -1,5 +1,8 @@
 package Java_B_2021;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -9,6 +12,25 @@ package Java_B_2021;
  */
 public class D {
     public static void main(String[] args) {
-
+        List<Long> list = new ArrayList<>();
+        int count = 0;
+        long n = 2021041820210418L;
+        long over = (long)Math.sqrt(n);
+        for(long i = 1; i <= over; i++){
+            if(n % i == 0){
+                list.add(i);
+                list.add(n/i);
+            }
+        }
+        for(int i = 0; i < list.size(); i++){
+            for(int j = 0; j < list.size(); j++){
+                for(int k = 0; k < list.size(); k++){
+                    if(list.get(i)* list.get(j)*list.get(k) == n){
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
