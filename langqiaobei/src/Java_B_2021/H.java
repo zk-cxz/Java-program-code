f6a68f6687f8ca99f94d7f9a203753b35e91f73c
蔡欣致 <2514029141@qq.com>
2022-08-13 23:12:53 +0800

    力扣牛客刷题测试代码提交

---
+++ langqiaobei/src/Java_B_2021/H.java
@@ -0,0 +1,12 @@
+package Java_B_2021;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: a
+ * Date: 2022-08-12
+ * Time: 19:50
+ */
+public class H {
+
+}
--- 力扣牛客刷题测试代码/src/AcWing周赛/Demo64_1.java
+++ 力扣牛客刷题测试代码/src/AcWing周赛/Demo64_1.java
@@ -1,5 +1,7 @@
 package AcWing周赛;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -8,5 +10,25 @@ package AcWing周赛;
  * Time: 18:58
  */
 public class Demo64_1 {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] array = new String[n];
+        int i = 0;
+        while(n-- > 0){
+            String str = scanner.next();
+            int len = str.length();
+            if(str.charAt(len-1) == 'o'){
+                array[i] = "FILIPINO";
+            }else if(str.charAt(len-1) == 'u'){
+                array[i] = "JAPANESE";
+            }else if(str.charAt(len-1) == 'a'){
+                array[i] = "KOREAN";
+            }
+            i++;
+        }
+        for(int j = 0; j < i; j++){
+            System.out.println(array[j]);
+        }
+    }
 }
